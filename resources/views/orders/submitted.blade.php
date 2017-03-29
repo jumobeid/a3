ffc70b73ccee519821f4cf786c6822c97738d75c
Jumana Obeid <color.ui>
2017-03-29 08:20:08 +0300

    fixed to retain values

--- resources/views/orders/submitted.blade.php
+++ resources/views/orders/submitted.blade.php
@@ -15,15 +15,15 @@
 
 	    <div class="container">
 		{!!Form::open(['url'=>'/orders/submitted','method'=>'POST','class'=>'form-horizantal'])!!}
-    {{csrf_field()}}
+			{{csrf_field()}}
 			<div class="form-group">
 			{!!Form::label('numberOfPeople','Number Of People:')!!}
-			{!!Form::number('numberOfPeople','$numberOfPeople',['class'=>'form-control'])!!}
+			{!!Form::number('numberOfPeople',$numberOfPeople,['class'=>'form-control'])!!}
 			</div>
 
 			<div class="form-group">
 			{!!Form::label('totalWithoutTip','Total Without Tip:')!!}
-			{!!Form::number('totalWithoutTip','$totalWithoutTip',['class'=>'form-control', 'step' => 'any'])!!}
+			{!!Form::number('totalWithoutTip',$totalWithoutTip,['class'=>'form-control', 'step' => 'any'])!!}
 			</div>
 
 			<div class="form-group">
